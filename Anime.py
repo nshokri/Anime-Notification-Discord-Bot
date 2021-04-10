ccdbee497c19cc53e503d16628cf308cecc380c2
Neema Shokri <noshokri@uw.edu>
2021-04-10 12:37:33 -0700

    removed setters

--- Anime.py
+++ Anime.py
@@ -3,21 +3,11 @@ class Anime:
 
     name = None
     crunchyroll_url = None
+    mal_url = None
     genres = None
     rating = None
     datetime_aired = None
 
-    # Constructor
     def __init__(self, name, crunchyroll_url):
         self.name = name
         self.crunchyroll_url = crunchyroll_url
-    
-    # Setters
-    def set_genres(self, genres):
-        self.genres = genres
-    
-    def set_rating(self, rating):
-        self.rating = rating
-    
-    def set_datetime_aired(self, datetime_aired):
-        self.datetime_aired = datetime_aired
