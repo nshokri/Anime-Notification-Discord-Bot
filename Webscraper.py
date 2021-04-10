7baa03367cdf97ef9272fd77e8cd7f8f1bad8b68
Nick <nshokri123@gmail.com>
2021-04-09 20:53:39 -0700

    webscraper

--- Webscraper.py
+++ Webscraper.py
@@ -1,2 +1,17 @@
-import requests
+from bs4 import BeautifulSoup
 
+with open('Crunchyroll.html', 'rb') as html_file:
+
+    content = html_file.read()
+
+    soup = BeautifulSoup(content, 'lxml')
+    all_anime_cards = soup.find_all('div', class_ = 'wrapper hover-toggle-queue container-shadow hover-classes')
+
+    for anime in all_anime_cards:
+
+        text = anime.text
+        index = 0
+        for i in range(8):
+            index = text.find(' ', index + 1)
+
+        print(text[0:index])
