fb0b0ba82a5cd19acac5c2a3c9762aaffd44fd58
Neema Shokri <noshokri@uw.edu>
2021-04-10 14:37:58 -0700

    added json setters/getters + genre arr

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 from time import strptime
 from datetime import datetime, date, time
 import re
-
+import json
 
 # Given an array of anime objects returns only those with genres in the desired filters
 def filter_by_genre(anime_list, filters):
@@ -27,3 +27,43 @@ def parse_datetime(aired, broadcast):
 
     result_datetime = datetime(year, month, day, hour, minute) - datetime(year, month, day, 14, 30) + datetime(year, month, day)
     return result_datetime
+
+def get_genre_arr():
+    test = "MusicMusic, Slice of LifeSlice of Life, DramaDrama, SchoolSchool, ShounenShounen"
+    genre_arr = test.split(", ")
+    print(genre_arr)
+    for i in range(0, len(genre_arr)):
+        genre = genre_arr[i]
+        genre_arr[i] = genre[0:int(len(genre) / 2)]
+    
+    return genre_arr
+
+def get_tracked():
+    f = open('config.json', 'r')
+    tracked = json.load(f)
+    return tracked
+
+def get_filters():
+    f = open('config.json', 'r') 
+    filters = json.load(f)
+    return filters
+
+def add_filter(filter):
+    f = open('config.json', 'r')
+    data = json.load(f)
+
+    if filter not in data["filters"]:
+        data["filters"].append(filter)
+
+    f = open('config.json', 'w')
+    json.dump(data, f)
+
+def remove_filter(filter):
+    f = open('config.json', 'r')
+    data = json.load(f)
+
+    if (filter in data["filters"]):
+        data["filters"].remove(filter)
+
+    f = open('config.json', 'w')
+    json.dump(data, f)
