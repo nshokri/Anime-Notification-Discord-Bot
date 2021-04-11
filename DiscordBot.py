b18d42544df2a2120e5fe157f2170d15301b3667
cshokri <cshokri@outlook.com>
2021-04-10 22:06:41 -0700

    added !help

--- DiscordBot.py
+++ DiscordBot.py
@@ -4,7 +4,7 @@ from Webscraper import Webscraper
 import time
 import helper
 
-REFRESH_RATE = 60
+REFRESH_RATE = 10
 CHANNEL_ID = 830636491582996490
 TOKEN = "ODMwMjM0MTA1ODk1MTkwNTU5.YHDtww.FiNA3g2KK3luTIXWz1QmLjgPhvQ"
 
@@ -18,7 +18,7 @@ async def on_ready():
     print("Bot is online")
     check_for_updates.start()
 
-@tasks.loop(seconds = 60)
+@tasks.loop(seconds = REFRESH_RATE)
 async def check_for_updates():
      filtered_anime = helper.filter_by_genre(anime_list, helper.get_filters())
      print("filtered: ", end=" ")
@@ -32,7 +32,7 @@ def create_embeded_message(name, genres, rating, link, day_aired, time_aired, la
     embed_var = discord.Embed(title="Episode " + latest_episode + " of \"" + name + "\" Just Came Out!", description="Watch here: " + link, color=0xF78C25)
     embed_var.add_field(name="__Day Aired__", value=day_aired, inline=True)
     embed_var.add_field(name="__Time Aired__", value=time_aired, inline=True)
-    embed_var.add_field(name="__Rating (" + str(float(rating) / 2) + "/5)__", value=round(float(rating) / 2) * "⭐", inline=True)
+    embed_var.add_field(name="__Rating (" + str(round(float(rating) / 2, 2)) + "/5)__", value=round(float(rating) / 2) * "⭐", inline=True)
     embed_var.add_field(name="__Genre__", value=genres, inline=True)
     embed_var.set_image(url = image)
     return embed_var
@@ -70,5 +70,11 @@ async def on_message(message):
             await message.channel.send("*No Longer Tracking:* " + "**" + anime_name + "**")
         else:
             await message.channel.send("**" + anime_name + "** *is not currently being tracked*")
+
+    elif command.startswith("!help"):
+        embed_var = discord.Embed(title="Current Command List", description="", color=0xF78C25)
+        embed_var.add_field(name="```!track [Anime Name]```", value="Tracks an anime and notifies the server when a new episode comes out", inline=True)
+        embed_var.add_field(name="```!untrack [Anime Name]```", value="Removes an anime from tracking list", inline=True)
+        await message.channel.send(embed = embed_var)
 
 client.run(TOKEN)
